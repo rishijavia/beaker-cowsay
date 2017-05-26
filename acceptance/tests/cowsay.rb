1c60028d3822c79b4b9733ea38e034b6907dcab6
Rishi Javia <rishi.javia.3895@gmail.com>
2017-05-26 14:59:28 -0700

    updated acceptance tests, still needs more work

--- acceptance/tests/cowsay.rb
+++ acceptance/tests/cowsay.rb
@@ -2,30 +2,29 @@ require 'beaker-cowsay'
 
 confine :to, :platform => 'ubuntu'
 
+# need to count characters of input to make -- and __ to wrap the quote
+
 def test_cow(quote)
-  return " _______
+  " _____________________
 < #{quote} >
- -------
-        \   ^__^
-         \  (oo)\_______
-            (__)\       )\/\
+ ---------------------
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
                 ||----w |
-                ||     ||"
+                ||     ||
+"
 end
 
 # Test cowsay
 test_name 'test cowsay' do
 
   hosts.each do |host|
-    cowsay(host, "i can mooo, can you?") do |result|
-      assert_equal(test_cow("i can mooo, can you?"), result)
-    end
+     assert_equal(test_cow("i can moo, can you?"), cowsay(host, "i can moo, can you?").stdout)
   end
 
   hosts.each do |host|
-    cowsay(host, 'i clean my input \n check it out') do |result|
-      assert_equal(test_cow('i clean my input n check it out'), result)
-    end
+    assert_equal(test_cow("i clean my input n check it out"), cowsay(host, "i clean my input \\n check it out").stdout)
   end
 
 end
